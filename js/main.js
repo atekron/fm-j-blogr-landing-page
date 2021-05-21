e633c1a0127b0d4d96b33da65467188eb3d00859
Michael Kret <dirtysculptor@gmail.com>
2021-05-21 20:03:51 +0300

    challenge done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+document
+  .querySelector(".header__hamburger-btn")
+  .addEventListener("click", function () {
+    let menu = document.querySelector(".header__hamburger-btn__open");
+    let close = document.querySelector(".header__hamburger-btn__close");
+    let mobileMenu = document.querySelector(".mobile-menu__links-wrapper");
+
+    if (menu.style.display !== "none") {
+      menu.style.display = "none";
+      close.style.display = "inline-block";
+      mobileMenu.style.display = "flex";
+    } else {
+      menu.style.display = "inline-block";
+      close.style.display = "none";
+      mobileMenu.style.display = "none";
+    }
+  });
